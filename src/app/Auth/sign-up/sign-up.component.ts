f3c40037b0b905ee523d0bc42f969c81ba9b8431
pranav vichare <pranav.vichare@tntra.io>
2023-03-20 11:55:37 +0530

    Add validations for login & signup

--- src/app/Auth/sign-up/sign-up.component.ts
+++ src/app/Auth/sign-up/sign-up.component.ts
@@ -6,18 +6,35 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './sign-up.component.html',
   styleUrls: ['./sign-up.component.scss']
 })
-export class SignUpComponent implements OnInit{
+export class SignUpComponent {
 
-  constructor(private formBuilder: FormBuilder) {}
-
-  signupForm!: FormGroup
-
-  ngOnInit(): void {
+  constructor(private formBuilder: FormBuilder) { 
     this.signupForm = this.formBuilder.group({
       email: ['', [Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
       password: ['', Validators.required],
       confirmPassword: ['', Validators.required]
+    },{
+      validator: this.ConfirmedValidator('password', 'confirmPassword')
     })
+  }
+
+  signupForm!: FormGroup
+
+  ConfirmedValidator(controlName: string, matchingControlName: string) {
+    return (formGroup: FormGroup) => {
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+      if (
+        matchingControl.errors && !matchingControl.errors['confirmedValidator']
+      ) {
+        return;
+      }
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ confirmedValidator: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+    };
   }
 
   getPasswordErrorMessage() {
@@ -26,16 +43,20 @@ export class SignUpComponent implements OnInit{
     }
 
     return this.signupForm.controls['password'].hasError('minlength') ? 'Password must be of atleast 6 character' : '';
-  } 
+  }
 
   getConfirmPasswordErrorMessage() {
     if (this.signupForm.controls['confirmPassword'].hasError('required')) {
       return 'Confirm password is Required!!';
     }
-    
+
     return this.signupForm.controls['confirmPassword'].hasError('minlength') ? 'confirm password must be of atleast 6 character' : '';
-  } 
-  
+  }
+
+  passwordDoesNotMatch() {
+    return 'Password does not match'
+  }
+
   getEmailErrorMessage() {
     if (this.signupForm.controls['email'].hasError('required')) {
       return 'Email is Required!!';
